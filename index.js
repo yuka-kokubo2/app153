bd0a8597107e0940a67274072aab2872d0037cd0
yuka-kokubo2 <167484694+yuka-kokubo2@users.noreply.github.com>
2024-08-30 14:24:53 +0900

    環境変数修正

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var request = require('request');
 /* kintone用のパラメータ（<>は要変更） */
 var DOMAIN = 'rjl9zaw66wf1.cybozu.com'; //kintone環境のドメイン
 var BASE_URL = "https://" + DOMAIN + '/k/v1/';
-var headers = {'X-Cybozu-API-Token': 'soydp78SxBjTyccwADXLc35bE4J9mGbildt7MYlQ'};
+var headers = {'X-Cybozu-API-Token': process.env.API_KEY};
 
 exports.handler = function(event, context) {
   var body_postrecords = {
@@ -36,10 +36,12 @@ exports.handler = function(event, context) {
   console.log(options_postrecords);
 
   try {
-    //kintoneのレコードを取得
+    //kintoneのレコードを登録
     request(options_postrecords, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        context.done(null, body.record);
+        context.done(null, "登録が完了しました！");
+      } else {
+        context.done(null, "登録に失敗しました");
       }
     });
   } catch (e) {
